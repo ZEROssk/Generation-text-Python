1dac718c80835790e89d6f724f246bf852d25161
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2019-02-14 13:33:04 +0900

    コードの整形

--- main.py
+++ main.py
@@ -6,89 +6,34 @@ import re
 from glob import iglob
 import inspect
 
-text_file = "./text_data/speech/asuka.txt"
+text_file_path = "./text_data/speech/asuka.txt"
 #text_file = "./text_data/speech/ALL.txt"
 
-#def read_file(filepath):
-#    text_data = ""
-#    for path in iglob(filepath):
-#        with open(path, 'r') as f:
-#            text_data += f.read().strip()
-#
-#    return text_data
-#
-#def text_wakati(text):
-#    t = MeCab.Tagger('-Owakati')
-#    m = t.parse(text)
-#    result = m.rstrip(' \n').split(' ')
-#
-#    return result
+def dic(file_path):
+    lines_data = open(file_path, "r").readlines()
+    markov_dic = {}
 
-#def first_word(sorce_data):
-#    with open(text_file) as f:
-#        data = f.read()
-#
-#    mecab = MeCab.Tagger()
-#    parse = mecab.parse(data)
-#    lines = parse.split("\n")
-#    items = (re.split("[\t,]", line) for line in lines)
-#
-#    words = [item[0]
-#            for item in items
-#            if (item[0] not in ('EOS', '', 't', 'ー') and
-#                item[1] == '名詞' and item[2] == '一般')]
-#
-#    f_word_dictionary = []
-#    print(sorce_data)
-#    for sorce in sorce_data:
-#        print(sorce)
-#        for comparison in words:
-#            if sorce in comparison:
-#                print('done')
-#                f_word_dictionary.append(sorce)
-#
-#    return f_word_dictionary
-
-def dic(fil):
-    data = open(text_file, "r")
-    lines = data.readlines()
-    markov = {}
-    for line in lines:
-        t = MeCab.Tagger('-Owakati')
-        m = t.parse(line)
-        result = m.rstrip(' \n').split(' ')
+    for line in lines_data:
+        mecab = MeCab.Tagger(
+                '-Owakati'
+                    ).parse(line).rstrip(' \n').split(' ')
         w1 = ""
-        w2 = ""        
-        for word in result:
+        w2 = ""
+
+        for word in mecab:
             if w1 and w2:
-                if (w1, w2) not in markov:
-                    markov[(w1, w2)] = []
-                markov[(w1, w2)].append(word)
+                if (w1, w2) not in markov_dic:
+                    markov_dic[(w1, w2)] = []
+                markov_dic[(w1, w2)].append(word)
             w1, w2 = w2, word
 
-        return markov
-    data.close()
-
-def main_dictionary(txt):
-    markov = {}
-    w1 = ""
-    w2 = ""
-
-    for word in txt:
-        if w1 and w2:
-            if (w1, w2) not in markov:
-                markov[(w1, w2)] = []
-            markov[(w1, w2)].append(word)
-        w1, w2 = w2, word
-
-    return markov
+    return markov_dic
 
 def markov_generate_text(markov):
     generate_text = ""
     w1 = ""
     w2 = ""
     count = 0
-    check = 0
 
     while True:
         w1, w2  = random.choice(list(markov.keys()))
@@ -110,6 +55,10 @@ def markov_generate_text(markov):
         elif '、' in w1:
             print('skip ',w1,w2)
             continue
+        elif '・' in w1:
+            print('skip ',w1,w2)
+            continue
+
         elif '！' in w2:
             print('skip ',w1,w2)
             continue
@@ -126,16 +75,13 @@ def markov_generate_text(markov):
             print('skip ',w1,w2)
             continue
         else:
-            break
-            generate_text += w1,w2
+            generate_text += w1
+            generate_text += w2
             print('generate ',generate_text)
-
-    #first_word_tmp = first_word(str(markov))
-    #print(first_word_tmp)
+            break
 
     while count < len(markov):
         tmp = random.choice(markov[(w1, w2)])
-        #print(tmp)
         generate_text += tmp
         w1, w2 = w2, tmp
 
@@ -150,18 +96,13 @@ def markov_generate_text(markov):
         elif '。' in tmp:
             break
 
-        count += 1
-
     return generate_text
 
 def main():
-    #read_text = read_file(text_file)
-    #base_text = text_wakati(read_text)
-    #dictionary = main_dictionary(base_text)
-    text = dic(text_file)
+    dic_text = dic(text_file_path)
 
-    #sentence = markov_generate_text(dictionary)
-    #print(sentence)
+    sentence = markov_generate_text(dic_text)
+    print(sentence)
 
     #for num in range(1000000):
     #    sentence = markov_generate_text(dictionary)
