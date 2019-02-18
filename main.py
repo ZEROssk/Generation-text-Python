86c22535069f9c28f10f8bf0da292df4410c18b4
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2019-02-18 12:41:59 +0900

    名詞辞書作成のベース完成

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 import random
 import MeCab
+import re
+import sys
 
 text_file_path = "./text_data/speech/asuka.txt"
 #text_file = "./text_data/speech/ALL.txt"
@@ -8,56 +10,82 @@ text_file_path = "./text_data/speech/asuka.txt"
 def dic(file_path):
     lines_data = open(file_path, "r").readlines()
     markov_dic = {}
+    words = []
 
     for line in lines_data:
         mecab = MeCab.Tagger(
-                '-Owakati'
+               '-Owakati'
                     ).parse(line).rstrip(' \n').split(' ')
-        w1 = ""
-        w2 = ""
 
-        for word in mecab:
-            if w1 and w2:
-                if (w1, w2) not in markov_dic:
-                    markov_dic[(w1, w2)] = []
-                markov_dic[(w1, w2)].append(word)
-            w1, w2 = w2, word
+        noun = MeCab.Tagger().parse(line).split('\n')
+        items = (re.split('[\t,]', line)for line in noun)
 
-    return markov_dic
+        for item in items:
+            if item[0] not in 'EOS' and item[1] == '名詞' and item[2] == '固有名詞':
+                print(item[0])
+        #words = [item[0]
+        #        for item in items
+        #        if (item[0] not in ('EOS', '', 't', 'ー') and
+        #            item[1] == '名詞')]
+    #for w in words:
+    #    print('名詞',w)
+    print(words)
 
-def markov_generate_text(markov):
-    generate_text = ""
-    w1 = ""
-    w2 = ""
-    count = 0
 
-    while True:
-        w1, w2  = random.choice(list(markov.keys()))
-        if '！' in w1 or '？' in w1 or '!' in w1 or '?' in w1 or '。' in w1 or '、' in w1 or '・' in w1:
-            print('skip ',w1,w2)
-            continue
-        elif '！' in w2 or '？' in w2 or '!' in w2 or '?' in w2 or '。' in w2:
-            print('skip ',w1,w2)
-            continue
-        else:
-            generate_text += w1
-            generate_text += w2
-            print('generate ',generate_text)
-            break
-
-    while True:#count < len(markov):
-        try:
-            tmp = random.choice(markov[(w1, w2)])
-        except KeyError:
-            print('KeyError')
-            break
-        generate_text += tmp
-        w1, w2 = w2, tmp
-
-        if '！' in tmp or '!' in tmp or '？' in tmp or '?' in tmp or '。' in tmp:
-            break
-
-    return generate_text
+#def dic(file_path):
+#    lines_data = open(file_path, "r").readlines()
+#    markov_dic = {}
+#
+#    for line in lines_data:
+#        mecab = MeCab.Tagger(
+#               '-Owakati'
+#                    ).parse(line).rstrip(' \n').split(' ')
+#        w1 = ""
+#        w2 = ""
+#
+#        for word in mecab:
+#            if w1 and w2:
+#                if (w1, w2) not in markov_dic:
+#                    markov_dic[(w1, w2)] = []
+#                markov_dic[(w1, w2)].append(word)
+#            w1, w2 = w2, word
+#
+#    return markov_dic
+#
+#def markov_generate_text(markov):
+#    generate_text = ""
+#    w1 = ""
+#    w2 = ""
+#    count = 0
+#
+#    while True:
+#        w1, w2  = random.choice(list(markov.keys()))
+#        if '！' in w1 or '？' in w1 or '!' in w1 or '?' in w1 or '。' in w1 or '、' in w1 or '・' in w1:
+#            print('skip ',w1,w2)
+#            continue
+#        elif '！' in w2 or '？' in w2 or '!' in w2 or '?' in w2 or '。' in w2:
+#            print('skip ',w1,w2)
+#            continue
+#        else:
+#            generate_text += w1
+#            generate_text += w2
+#            print('generate ',generate_text)
+#            break
+#
+#    while True:#count < len(markov):
+#        try:
+#            tmp = random.choice(markov[(w1, w2)])
+#        except KeyError:
+#            print('KeyError')
+#            break
+#
+#        generate_text += tmp
+#        w1, w2 = w2, tmp
+#
+#        if '！' in tmp or '!' in tmp or '？' in tmp or '?' in tmp or '。' in tmp:
+#            break
+#
+#    return generate_text
 
 def main():
     dic_text = dic(text_file_path)
@@ -65,9 +93,9 @@ def main():
     #sentence = markov_generate_text(dic_text)
     #print(sentence)
 
-    for num in range(1000):
-        sentence = markov_generate_text(dic_text)
-        print(num,sentence)
+    #for num in range(1000):
+    #    sentence = markov_generate_text(dic_text)
+    #    print(num,sentence)
 
 if __name__ == "__main__":
     main()
