9c5021de979a1902d26025fa46ad132b4d45b53f
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2019-02-26 15:25:25 +0900

    move input file name and file path

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import os
 import json
 import ast
 
+data_set_name = input('Pleas directory name: ')
+main_dic_path = data_set_name + '/' + data_set_name + '_main.json'
+noun_dic_path = data_set_name + '/' + data_set_name + '_noun.txt'
 #text_file_path = "./text_data/speech/asuka.txt"
 #text_file_path = "./text_data/speech/ALL.txt"
 
-def read_dictionary(data_name):
-    main_dic_path = data_name + '/' + data_name + '_main.txt'
-    noun_dic_path = data_name + '/' + data_name + '_noun.txt'
-
+def read_dictionary():
     with open(main_dic_path) as f:
         read_main_dic = json.load(f)
         #rmd = f.read()
@@ -29,14 +29,10 @@ def read_dictionary(data_name):
 
     return read_main_dic, read_noun_dic
 
-def write_dictionary_data(data_name, name):
-    main_dic_path = name + '/' + name + '_main.json'
-    noun_dic_path = name + '/' + name + '_noun.txt'
-
+def write_dictionary_data(data_name):
     with open(main_dic_path, 'w') as f:
         t = str(data_name[0])
         test = {"test":t}
-        print(test)
         json.dump(test, f, indent=4)
         #f.write(str(main_dic))
 
@@ -45,7 +41,7 @@ def write_dictionary_data(data_name, name):
 
     return write_main_dic, write_noun_dic
 
-def make_dictionary(file_path, data_name):
+def make_dictionary(file_path):
     lines_data = open(file_path, "r").readlines()
     main_dic = {}
     noun_dic = []
@@ -117,17 +113,16 @@ def markov_generate_text(dictionaries):
     return generate_text
 
 def main():
-    data_set_name = input('Pleas directory name: ')
     if os.path.exists(data_set_name):
-        dic = read_dictionary(data_set_name)
+        dic = read_dictionary()
         #print('read data')
         #sentence = markov_generate_text(dic)
         #print(sentence)
     else:
         os.mkdir(data_set_name)
         text_file_path = input('Pleas txt file path: ')
-        dic = make_dictionary(text_file_path, data_set_name)
-        write_dic = write_dictionary_data(dic, data_set_name)
+        dic = make_dictionary(text_file_path)
+        write_dic = write_dictionary_data(dic)
         #sentence = markov_generate_text(dic)
         #print(sentence)
 
